df16ea5372e8e111e77a80ff84aa29544489ceb9
amitihere <amiti.aneesh1@gmail.com>
2026-01-28 22:38:37 +0530

    basic folder creation in frontend

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { Link } from '@react-navigation/native';
+
+export default function Login(){
+    return (
+        <>
+            <div>
+                <h1>This is the login page</h1>
+            </div>
+        </>
+    )
+}
