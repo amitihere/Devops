8f1f54a91f2f1cb1efb054f4d4c9613ffbe5f939
amitihere <amiti.aneesh1@gmail.com>
2026-03-02 16:19:29 +0530

    added cart page / component

---
+++ client/src/components/Navbar/Shopcart.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Shopcart.css';
+
+export default function Shopcart() {
+  return (
+    <div className="shopcart">
+      <h2>Shopcart</h2>
+      <p>Your shopping cart is currently empty.</p>
+    </div>
+  );
+}
